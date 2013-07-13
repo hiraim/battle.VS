270e533f735ed252d9f0a9f84e1c2f53f93d1356
hiraim <b240069@cuc.ac.jp>
2013-07-13 11:57:53 +0900

    画像の表示　#4

--- MyGame/script.js
+++ MyGame/script.js
@@ -1,41 +1,16 @@
 enchant();
-window.onload = function() {
-    //ゲームオブジェクトの生成
-    var game = new Game(320, 320);
-    game.fps = 16;
 
-    //画像の読み込み
-    game.preload('http://enchantjs.com/assets/images/chara1.gif');
-
-    //ロード完了時に呼ばれる
-    game.onload = function() {
-        //スプライトの生成
-        var bear   = new Sprite(32, 32);
-        bear.image = game.assets['http://enchantjs.com/assets/images/chara1.gif'];
-        bear.tick  = 0;
-        bear.anim  = [0, 1, 0, 2];
-        game.rootScene.addChild(bear);
-        //スプライトの定期処理
-        bear.addEventListener(Event.ENTER_FRAME, function() {
-            //スプライトのフレームの指定
-            bear.tick++;
-            bear.frame = bear.anim[bear.tick % 4];
-            
-            //右向き
-            if (bear.scaleX == 1) {
-            bear.x += 3;
-                //向き変更
-                if (bear.x > 320 - 32) bear.scaleX = -1;
-            } 
-            //左向き
-            else {
-                bear.x -= 3;
-                //向き変更
-                if (bear.x < 0) bear.scaleX = 1;
-            }
-        });
+window.onload = function(){
+    var game = new Game(320,320);
+    
+    game.preload('images/chara9.png');
+    
+    game.onload = function(){
+        var human = new Sprite(50,55);
+        human.image = 
+            game.assets['images/chara9.png'];
+      
+        game.rootScene.addChild(human);
     };
-
-    //ゲームの開始
     game.start();
 };
