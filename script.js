1171fc0f39935ce6cb97fc7c9cd230ac149805c6
hiraim <b240069@cuc.ac.jp>
2013-07-14 10:34:58 +0900

    #9
    アイコンクリックで敵のHP減少。("攻撃","いかずち")

--- script.js
+++ script.js
@@ -9,27 +9,8 @@ window.onload = function(){
     label.x = 20;
     label.y = 20;
 
-    /*
-    var kougekico = new Sprite(150,150);
-    
-    kougekico.x = 120;
-    kougekico.y = 120;
-    
-    kougekico.addEventListener(Event.TOUCH_START,function(e){
-        score.toX = e.x - 16;
-       
-        /*
-        var kougekico = new Sprite(100,50);
-        kougekico.image = game.assets['images/kougekico.jpeg'];
-        kougekico.x = 0;
-        kougekico.y = 210;
-        kougekico.scaleX = 0.5;
-        kougekico.scaleY = 0.5;
-        */
-    /*
-    });
-*/
-    game.preload('images/chara9.png','images/chara10.jpeg','images/back.jpeg','images/icon.jpeg','images/kougekico.jpeg');
+   
+ game.preload('images/chara9.png','images/chara10.jpeg','images/back.jpeg','images/icon.jpeg','images/kougekico.jpeg','images/thunderstormco.jpeg','images/guardco.jpeg','images/shirkco.jpeg');
     
     game.onload = function(){
         var human = new Sprite(200,200);
@@ -39,6 +20,7 @@ window.onload = function(){
         human.scaleX = 0.3;
         human.scaleY = 0.3;
       
+        
         var satan = new Sprite(70,100);
         satan.image = 
             game.assets['images/chara10.jpeg'];
@@ -49,7 +31,7 @@ window.onload = function(){
         
         
         var bg = new Sprite(320,320);
-      bg.image = game.assets["images/back.jpeg"];
+       bg.image = game.assets["images/back.jpeg"];
         
         
         var icon = new Sprite(200,240);
@@ -59,20 +41,53 @@ window.onload = function(){
         icon.y = 140;
         icon.scaleX = 0.5;
         icon.scaleY = 0.5;
+
         
     var kougekico = new Sprite(100,50);
         kougekico.image = 
             game.assets['images/kougekico.jpeg'];
         kougekico.x = 0;
-        kougekico.y = 210;
-        kougekico.scaleX = 0.5;
-        kougekico.scaleY = 0.5;
-        
+        kougekico.y = 208;
+        kougekico.scaleX = 0.4;
+        kougekico.scaleY = 0.4;
+    
         kougekico.addEventListener(Event.TOUCH_END,function(e){
-           console.log("ma");
+          
             score -= 10;
             label.text = "SCORE:"+score;
         });
+        
+        
+        var thunderstormco = new Sprite(170,50);
+        thunderstormco.image = 
+            game.assets['images/thunderstormco.jpeg'];
+        thunderstormco.x = -28;
+        thunderstormco.y = 231;
+        thunderstormco.scaleX = 0.4;
+        thunderstormco.scaleY = 0.4;
+        
+        thunderstormco.addEventListener(Event.TOUCH_END,function(e){
+          
+            score -= 10;
+            label.text = "SCORE:"+score;
+        
+        });
+            
+        var guardco = new Sprite(150,50);
+        guardco.image = 
+            game.assets['images/guardco.jpeg'];
+        guardco.x = -14;
+        guardco.y = 248;
+        guardco.scaleX = 0.4;
+        guardco.scaleY = 0.4;
+        
+    var shirkco = new Sprite(150,50);
+        shirkco.image = 
+            game.assets['images/shirkco.jpeg'];
+        shirkco.x = -20;
+        shirkco.y = 268;
+        shirkco.scaleX = 0.4;
+        shirkco.scaleY = 0.4;
         
         /*
         kougekico.ontotchend = function(){
@@ -87,6 +102,10 @@ window.onload = function(){
         game.rootScene.addChild(label);
         game.rootScene.addChild(icon);
         game.rootScene.addChild(kougekico);
+        game.rootScene.addChild(thunderstormco);
+        game.rootScene.addChild(guardco);
+        game.rootScene.addChild(shirkco);
+   
     };
     
     game.start();
